c9f66f95cc1d1e5f6ae3b1ed60555769835927c5
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-05-28 04:11:39 +0300

    Improvements

--- src/components/Assortment.vue
+++ src/components/Assortment.vue
@@ -1,3 +1,7 @@
+<script setup>
+import MyButton from "./MyButton.vue";
+</script>
+
 <template>
   <div class="row mt-5">
     <div class="col-12 text-center main-title">
@@ -21,73 +25,144 @@
 
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">grain coffee</div>
-              <div class="card-description">Now you can buy alertness! 20 types of drinks, alternative milk options </div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">grain coffee</div>
+                  <div class="card-description">Now you can buy alertness! 20 types of drinks, alternative milk options </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">breakfasts</div>
-              <div class="card-description">With self. early mornings at work won't be so heavy </div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">grain coffee</div>
+                  <div class="card-description">Now you can buy alertness! 20 types of drinks, alternative milk options </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4 mt-md-2 mt-lg-4">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">main courses</div>
-              <div class="card-description">Ready-made dishes from the best suppliers</div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">main courses</div>
+                  <div class="card-description">Ready-made dishes from the best suppliers</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4 mt-md-2 mt-xl-4">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">filling snacks</div>
-              <div class="card-description">Fresh sandwiches, shawarma and other snacks - we will not leave you hungry</div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">filling snacks</div>
+                  <div class="card-description">Fresh sandwiches, shawarma and other snacks - we will not leave you hungry</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4 mt-md-2">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">organic and healthy</div>
-              <div class="card-description">Protein bars and other healthy food without leaving the office.</div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">organic and healthy</div>
+                  <div class="card-description">Protein bars and other healthy food without leaving the office.</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4 mt-md-2">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">snacks and bars</div>
-              <div class="card-description">Favorite chocolates, snacks, nuts, and other quick snacks</div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">snacks and bars</div>
+                  <div class="card-description">Favorite chocolates, snacks, nuts, and other quick snacks</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4 mt-md-2">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">refreshing drinks</div>
-              <div class="card-description">No need to leave the office to buy a cold Pepsi or energy drink anymore</div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">refreshing drinks</div>
+                  <div class="card-description">No need to leave the office to buy a cold Pepsi or energy drink anymore</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
         <div class="col-12 col-md-6 col-lg-4 col-xl-3 p-3 mt-4 mt-md-2">
           <div class="card">
-            <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <div class="card-title">ice cream</div>
-              <div class="card-description">Cool down even on the hottest day with delicious ice cream</div>
+            <div class="row">
+              <div class="col-6 col-md-12">
+                <div class="card-image">
+                  <img src="../assets/card-coffee.png" class="card-img-top" alt="...">
+                </div>
+              </div>
+              <div class="col-6 col-md-12 ps-0 ps-md-2">
+                <div class="card-body">
+                  <div class="card-title mt-2 mt-md-0">ice cream</div>
+                  <div class="card-description">Cool down even on the hottest day with delicious ice cream</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -108,14 +183,6 @@
   </div>
 
 </template>
-
-<script>
-import MyButton from "./MyButton.vue";
-export default {
-  name: "Assortment",
-  components: {MyButton}
-}
-</script>
 
 <style scoped>
 .main-title {
@@ -147,12 +214,14 @@ export default {
 }
 
 .card-body {
-  height: 137px;
+  min-height: 137px;
 }
 
-.card > img {
+.card-image > img {
   padding: 22px;
+  min-height: 166px;
   background-color: #FFC54A;
+  border-radius: 20px 0 0 20px;
 }
 
 .card-title {
@@ -166,5 +235,20 @@ export default {
   font-weight: 400;
   line-height: 19px;
   text-align: left;
+}
+
+@media only screen and (min-width: 768px) {
+  .card {
+    border-radius: 20px;
+    --bs-card-inner-border-radius: 20px;
+  }
+
+  .card-body {
+    height: 137px;
+  }
+
+  .card-image > img {
+    border-radius: 20px 20px 0 0;
+  }
 }
 </style>
