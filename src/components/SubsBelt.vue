5eaa1354b1a55ee6a7ad3e880c28bbd61a3b14e0
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-05-28 06:51:51 +0300

    Improvements

---
+++ src/components/SubsBelt.vue
@@ -0,0 +1,108 @@
+<script setup>
+</script>
+
+<template>
+<div class="row d-flex justify-content-center main-row mt-5">
+  <div class="col-11 col-md-12 col-lg-10 main-col">
+    <div class="row">
+
+      <div class="col-12 col-md-6">
+        <div class="title-box">
+          <div class="title mt-5 pe-lg-4">
+            We are the developers of our own Self.Subsidy program.
+          </div>
+          <!-- Bg INDEX -->
+          <div class="d-none d-lg-inline-block">
+            <img alt="#" class="bg-index-drop-2" src="../assets/subsidy/card-2.png" height="110">
+          </div>
+        </div>
+        <!-- Bg INDEX -->
+        <div class="d-lg-none">
+          <img alt="#" class="bg-index-drop" src="../assets/subsidy/card.png" height="70">
+        </div>
+      </div>
+      <div class="col-12 col-md-6 mt-md-5 ps-lg-5">
+        <div class="info mt-md-3 pt-lg-2">
+          Self.supsidy allows companies to flexibly manage the subsidy provided and gives employees the opportunity to use the subsidy in our micromarkets at their discretion.
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.main-row {
+  background-color: white;
+}
+
+.main-col {
+  max-width: 1600px;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 900;
+  line-height: 32px;
+  text-align: left;
+  color: white;
+  padding: 24px 5px 19px 24px;
+}
+
+.title-box {
+  background-color: #00C3B2;
+  border-radius: 20px;
+  height: 151px;
+  z-index: 1;
+}
+
+.info {
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 29px;
+  text-align: left;
+}
+
+.bg-index-drop {
+  position: absolute;
+  right: 0;
+  z-index: 0;
+  margin-top: -70px;
+}
+
+@media only screen and (min-width: 768px) {
+  .bg-index-drop {
+    left: 37vw;
+    z-index: 0;
+  }
+}
+
+@media only screen and (min-width: 992px) {
+  .bg-index-drop-2 {
+    position: absolute;
+    left: 43vw;
+    z-index: 0;
+    margin-top: -170px;
+  }
+}
+@media only screen and (min-width: 1200px) {
+  .title {
+    position: relative;
+    font-size: 32px;
+    line-height: 44px;
+    max-width: 433px;
+  }
+  .info {
+    font-size: 20px;
+    line-height: 28px;
+  }
+  .title-box {
+    height: auto;
+  }
+  .bg-index-drop-2 {
+    left: 45vw;
+    margin-top: -220px;
+  }
+}
+</style>
---
+++ src/components/SubsContent.vue
@@ -0,0 +1,90 @@
+<script setup>
+
+import MyButton from "./MyButton.vue";
+</script>
+<template>
+  <div class="row d-flex justify-content-center">
+    <div class="col-11 col-md-10">
+
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <h1 class="title"><span>Subsidy system</span></h1>
+          <div class="info mt-5">
+            make your employees more productive and happier
+          </div>
+          <div class="additional-info mt-4">
+            Connection of the subsidy system is an additional but not mandatory service. All connection terms are discussed individually.
+          </div>
+          <div class="mt-4 pt-3">
+            <MyButton text="Leave an application" class="px-4"/>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6 text-center">
+          <div class="image">
+            <img src="../assets/subsidy/img.png" alt="subsidy" class="ms-0 ms-lg-5">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  color: black;
+}
+.title {
+  font-weight: 800;
+  font-size: 28px;
+  line-height: 34px;
+  margin-top: 130px;
+}
+
+.title > span {
+  border-bottom: 6px solid black;
+}
+
+.info {
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 33px;
+  text-align: left;
+}
+
+.additional-info {
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 25px;
+  text-align: left;
+  max-width: 500px;
+}
+
+.image > img{
+  margin-top: 33px;
+  width: 100%;
+  max-width: 540px;
+}
+
+@media only screen and (min-width: 992px) {
+  .title {
+    margin-top: 191px;
+    font-size: 40px;
+    line-height: 40px;
+  }
+
+  .info {
+    font-size: 32px;
+    line-height: 45px;
+  }
+
+  .additional-info {
+    font-size: 20px;
+    line-height: 28px;
+  }
+
+  .image > img{
+    margin-top: 233px;
+  }
+}
+</style>
