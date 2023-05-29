f3134b7a1f729ecda2a07daf9f56574077d86e13
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-05-29 10:44:37 +0300

    Improvements

--- src/components/Assortment.vue
+++ src/components/Assortment.vue
@@ -5,7 +5,7 @@ import {saveAs} from 'file-saver';
 const download = (url) => {
   axios.get(url, {responseType: 'blob'})
     .then(response => {
-      saveAs(response.data, 'Abdullokh Khamzaev');
+      saveAs(response.data, 'Product range');
     })
 }
 </script>
@@ -181,7 +181,7 @@ const download = (url) => {
               <MyButton text="I want a free tasting" background="#6357AD" class="me-1 w-100" max-width="396" />
             </div>
             <div class="col-12 col-md-6 text-center text-md-start mt-4 mb-4 mt-md-0">
-              <MyButton text="download the assortment" @click="download('CV.pdf')" class="w-100 ms-1" max-width="396" />
+              <MyButton text="download the assortment" @click="download('assortment.pdf')" class="w-100 ms-1" max-width="396" />
             </div>
           </div>
         </div>
