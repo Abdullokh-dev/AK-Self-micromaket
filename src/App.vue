6a1d1ac2106a948b208cde9879a66cb90d787e0c
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-05-29 18:49:45 +0300

    foo

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,23 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import MyButton from "./components/MyButton.vue";
-
-const foo = () => {
-  $.ajax({
-    url: '/mail.php'
-  });
-}
+import {ref} from "vue";
+const name = ref('');
+const phone = ref('');
+const email = ref('');
+const comment = ref('');
+const sendMsg = () => Email.send({
+  Username : "hamzayevabdulloh452@gmail.com",
+  Password : "FAEA429B5CF0038E98805D7255EE6E04FE63",
+  Host: "smtp.elasticemail.com",
+  To : 'abdulloh3525dev@gmail.com',
+  From : "hamzayevabdulloh452@gmail.com",
+  Subject : "Application from self-micromarket.com: instalation",
+  Body : '<h1>' + name.value + '</h1>' + '<h2>' + phone.value + '</h2>' + '<h2>' + email.value + '</h2>' + '<h3>' + comment.value + '</h3>'
+}).then( () => {
+    alert("sent successfully")
+  }
+);
 </script>
 
 <template>
@@ -22,24 +33,24 @@ const foo = () => {
           <button type="button" class="btn-close pt-4 pe-4" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body pt-0">
-          <form class="was-validated text-center">
+          <form class="was-validated text-center" @submit.prevent="sendMsg">
             <div class="modal-title">
               leave an application
             </div>
             <div class="px-4 mt-3">
-              <input type="text" id="name" placeholder="Name" class="ps-3 form-control" required />
+              <input type="text" id="name" placeholder="Name" class="ps-3 form-control" required v-model="name" />
             </div>
 
             <div class="px-4 mt-3">
-              <input type="text" placeholder="Phone" class="ps-3 form-control" required />
+              <input type="text" placeholder="Phone" class="ps-3 form-control" required v-model="phone"/>
             </div>
 
             <div class="px-4 mt-3">
-              <input type="email" placeholder="Email" class="ps-3 form-control" required />
+              <input type="email" placeholder="Email" class="ps-3 form-control" required v-model="email"/>
             </div>
 
             <div class="px-4 mt-3">
-              <input type="text" placeholder="Comment" class="ps-3 form-control" required />
+              <input type="text" placeholder="Comment" class="ps-3 form-control" required v-model="comment" />
             </div>
 
             <div class="d-flex justify-content-start mt-3">
@@ -50,7 +61,7 @@ const foo = () => {
             </div>
 
             <div class="mt-4">
-              <MyButton text="Send" type="submit" class="my-3 px-4" @click="foo"/>
+              <MyButton text="Send" type="submit" class="my-3 px-4"/>
             </div>
           </form>
         </div>
