56c1d0367257dc293fefb599d46b86daaffcf355
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-06-01 16:01:38 +0300

    Improvements

--- src/pages/Deliver.vue
+++ src/pages/Deliver.vue
@@ -1,10 +1,16 @@
 <script setup>
-
+import {onMounted} from "vue";
 import DeliveryContent from "../components/DeliveryContent.vue";
 import DeliveryBelt from "../components/DeliveryBelt.vue";
 import DeliveryOffer from "../components/DeliveryOffer.vue";
 import DeliverSecondBelt from "../components/DeliverSecondBelt.vue";
 import Header from "../components/Header.vue";
+
+const scrollTop = () => {
+  window.scrollTo(0,0);
+}
+
+onMounted(scrollTop)
 </script>
 
 <template>
