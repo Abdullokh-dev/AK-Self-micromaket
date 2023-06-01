3386ea79df82784d42ffca51df46913a783cf7b8
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-06-01 04:07:05 +0300

    Disable hide Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,16 +9,16 @@ $(window).on('click', function(event){
   }
 });
 
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100%";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-100%";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
 const clicked = ref(false)
 
